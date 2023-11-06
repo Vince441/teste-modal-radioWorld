2d9e63c71d1cd22297b0077e11ca1bdfd0027d41
Vince441 <vincentlouvart82@gmail.com>
2023-11-06 23:42:54 +0100

    ajout d'un evenement qui empèche la fenêtre de se fermer lorsque je clique sur la radio

--- src/components/ModalContent.jsx
+++ src/components/ModalContent.jsx
@@ -2,15 +2,21 @@ import React from 'react'
 
 
 export default function ModalContent({ closeModal }) {
+  const handleIntContentClick = (e) => {
+    e.stopPropagation();
+  };
+
+  
   return (
     <>
-    <div  className='content'></div>
+    
     <div onClick={closeModal} className='container-radio'>
     
-    <div className='int-content'>
+    <div className='int-content' onClick={handleIntContentClick}>
     <button onClick={closeModal} className='button'>X</button>
     </div>
     </div>
+    
     </>
   )
 }
